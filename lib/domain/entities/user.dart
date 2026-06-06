ba208a786780316eb25a007a14df4c3ffcccd2d5
Thèn Scu <bimapbeo1001@gmail.com>
2026-06-06 15:05:45 +0700

    clean duplication

--- lib/domain/entities/user.dart
+++
@@ -1,6 +0,0 @@
-class User {
-  const User({required this.id, required this.name});
-
-  final String id; // Unique user ID (UUID or timestamp)
-  final String name; // Username/Display name
-}
--- lib/presentation/providers/movie_detail_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../domain/entities/movie.dart';
-import 'app_providers.dart';
-
-final movieDetailProvider = FutureProvider.family<Movie, int>((ref, movieId) {
-  return ref.read(getMovieDetailUseCaseProvider).call(movieId);
-});
